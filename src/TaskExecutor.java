dbb7378bed0a3f28d255f88983712320e574d627
Lengl <Lengl@yandex.ru>
2015-08-18 02:27:39 +0300

    Replaced semaphore with .wait() on stack.

--- src/TaskExecutor.java
+++ src/TaskExecutor.java
@@ -1,26 +1,23 @@
 import java.util.Stack;
-import java.util.concurrent.Semaphore;
 
 /**
  * Created by Lengl on 17.08.2015.
  */
 public class TaskExecutor extends Thread {
+    private static final int localStackCapacity = 30;
+    private static final Task terminatorTask = Task.noTasks;
     //Any calls to this variables should be synchronized!
     private static double result;
     private static Stack <Task> globalStack;
-    private static final int localStackCapacity = 30;
     private static int workingThreadsCount;
     private static int livingThreadsCount = 0;
-    //this semaphore represents amount of available tasks in globalStack
-    private static Semaphore globalStackLocker;
-    private static final Task terminatorTask = Task.noTasks;
 
     private Stack <Task> localStack;
 
     //This one actually duplicates pushInGlobalStack, I'm not sure if it should exist.
     public static void pushTask(Task task) {
         globalStack.push(task);
-        globalStackLocker.release();
+        globalStack.notify();
     }
 
     public void run() {
@@ -62,19 +59,20 @@ public class TaskExecutor extends Thread {
 
     private synchronized Task popFromGlobalStack() {
         workingThreadsCount--;
+        //If we finished
         if (workingThreadsCount == 0 && globalStack.isEmpty()){
             for (int i = 0; i < livingThreadsCount; i++) {
                 globalStack.push(terminatorTask);
             }
-            globalStackLocker.release(livingThreadsCount);
+            globalStack.notifyAll();
             //here we are saying to the externals that result is ready
             this.notifyAll();
         } else {
             try {
-                globalStackLocker.acquire();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+                //wait until global stack refilled
+                while (globalStack.isEmpty())
+                    globalStack.wait();
+            } catch (InterruptedException e) {}
             workingThreadsCount++;
         }
         return globalStack.pop();
@@ -82,7 +80,7 @@ public class TaskExecutor extends Thread {
 
     private synchronized void pushInGlobalStack(Task task) {
         globalStack.push(task);
-        globalStackLocker.release();
+        globalStack.notify();
     }
 
     private synchronized void addToResult (double summand) {
